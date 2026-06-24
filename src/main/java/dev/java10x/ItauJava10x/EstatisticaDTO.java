d278e7a4a391f41b2044f42ff87ff3373a3f78dc
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2026-06-24 13:49:45 +0100

    feat: add transaction creation endpoint and data models

---
+++ src/main/java/dev/java10x/ItauJava10x/EstatisticaDTO.java
@@ -0,0 +1,14 @@
+package dev.java10x.ItauJava10x;
+
+import lombok.Getter;
+
+@Getter
+public class EstatisticaDTO {
+
+    private long count;
+    private double sum;
+    private double avg;
+    private double min;
+    private double max;
+
+}
--- src/main/java/dev/java10x/ItauJava10x/TransacoesController.java
+++ src/main/java/dev/java10x/ItauJava10x/TransacoesController.java
@@ -1,7 +1,20 @@
 package dev.java10x.ItauJava10x;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacao")
 public class TransacoesController {
 
+    @PostMapping
+    public ResponseEntity adicionar() {
 
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
 
 }
